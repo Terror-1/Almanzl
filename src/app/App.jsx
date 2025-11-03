4811dee2fca32ee7d2098214b62c2a63be8e51cb
yousefmobelal <ym8370302@gmail.com>
2025-11-03 11:33:01 +0200

    Merge product details into development

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,10 +5,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 import AppRoutes from "./app.routes";
 import { AuthProvider } from "../pages/authentication/context/AuthProvider";
-import Navbar from "../components/Navbar";
-import SubNav from "../components/SubNav";
-import ProductDetailsPage from "../pages/productDetails/ProductDetailsPage";
-import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
@@ -18,12 +14,6 @@ function App() {
         <AppRoutes />
       </AuthProvider>
     </Router>
-    <>
-      <ToastContainer position="top-right" autoClose={3000} />
-      <Navbar />
-      <ProductDetailsPage />
-      <SubNav />
-    </>
   );
 }
 export default App;
